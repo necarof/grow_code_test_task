8d905b88fbb0c29b05a9b2fad775382d7e37b5e2
Nikita <nikitampost@gmail.com>
2025-02-27 11:03:15 +0500

    add content

---
+++ script.js
@@ -0,0 +1,20 @@
+"use strict";
+
+document.addEventListener('DOMContentLoaded', () => {
+    const form = document.getElementById('form');
+    form.addEventListener('submit', formSend);
+
+    async function formSend(e) {
+        e.preventDefault();
+
+        let error = formValidate(form);
+    }
+
+    function formValidate(form) {
+        let error = 0;
+        let formReq = document.querySelectorAll('._req');
+    } 
+
+    console.log(form);
+    
+})
